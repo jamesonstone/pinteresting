e0fd1bcffc3ec0ab096e57f78fa855a24087ad7b
Gabriele O'Connor <oconnor.gabriele@gmail.com>
2014-10-07 17:21:05 -0400

    Creating home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pinteresting::Application.routes.draw do
+ get "pages/home"
+ get "about" => "pages@about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
